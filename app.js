adb0db24849403a7d1f18674b0a8ae6836416b5e
Zahar Gulchak <gulchak.zahar@gmail.com>
2015-06-16 23:48:31 +0300

    Implemented Angular code instead of Vanilla JS

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ var app = express();
 var logger = require('morgan');
 app.use(logger('combined'));
 
-app.use('/', express.static('public'));
-
-app.get('/test', function(request, response) {
-    response.send('Hello World!');
-});
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
