936ac5c00485e59b41c65c28df280dd57f828877
Zakhar Gulchak <z.gulchak@levi9.com>
2015-06-17 10:33:58 +0300

    Small fixes

--- public/js/core.js
+++ public/js/core.js
@@ -2,8 +2,6 @@ angular.module('pipeline', [])
     .controller('mainController', ['$scope', '$http',
 
 function mainController($scope, $http) {
-    $scope.formData = {};
-
     // when landing on the page, get all todos and show them
     $http.get('http://pipes.yahoo.com/pipes/pipe.run?_id=e9a2e77dffb3205d035c4e311d77bbe6&_render=json')
         .success(function(data) {
